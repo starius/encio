d5a5e6f49b35973345da54361fd5a86f1770e76c
Boris Nagaev <bnagaev@gmail.com>
2017-03-22 07:02:14 +0000

    add example: hello

---
+++ hello/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/starius/encio"
+)
+
+func main() {
+	block, err := aes.NewCipher(make([]byte, 16))
+	if err != nil {
+		log.Fatalf("Failed to make AES: %s", err)
+	}
+	aead, err := cipher.NewGCM(block)
+	if err != nil {
+		log.Fatalf("Failed to make AEAD: %s", err)
+	}
+	backend, err := ioutil.TempFile("", "test-encio-hello-")
+	if err != nil {
+		log.Fatalf("Failed to create temp file: %s.", err)
+	}
+	defer os.Remove(backend.Name())
+	defer backend.Close()
+	if err != nil {
+		log.Fatalf("Failed to open backend: %s", err)
+	}
+	frontend := encio.NewAppender(aead, backend, 4096)
+	_, err = frontend.WriteAt([]byte("Hello"), 0)
+	if err != nil {
+		log.Fatalf("Failed to write: %s", err)
+	}
+	_, err = frontend.WriteAt([]byte(" world"), 5)
+	if err != nil {
+		log.Fatalf("Failed to write: %s", err)
+	}
+	b := make([]byte, 11)
+	_, err = frontend.ReadAt(b, 0)
+	if err != nil {
+		log.Fatalf("Failed to write: %s", err)
+	}
+	fmt.Println(string(b))
+}
